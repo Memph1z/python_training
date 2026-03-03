38cde795121c3a02562ec04b392dd0b1b2dc33c2
sousFlex <memph1zgray@gmail.com>
2026-03-04 00:54:49 +0300

    Fixed tests

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -4,5 +4,5 @@ from model.contact import Contact
 
 def test_edit_contact(app):
     app.session.login("admin", "secret")
-    app.contact.edit_first_contact("This is a test")
+    app.contact.edit_first_contact(Contact("123", "123", "123", "123", "123", "123", "123", "123", "123", "123", "123", "123", "123", "123", "6", "July", "2000", "2", "August", "2001"))
     app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,5 +2,5 @@
 
 def test_edit_first_group(app):
     app.session.login("admin", "secret")
-    app.group.edit_first_group("This is a test")
+    app.group.edit_first_group(Group(name="Test group", header="Test header", footer="Test footer"))
     app.session.logout()
